79c548f7129d4be593c6c36ae42666d61e45faba
Francesco Pezzini <francesco.pezzini87@gmail.com>
2020-02-27 21:41:04 +0100

    thresh_nucleosomes scripts, commented, but some info to add

--- neptuner/thresh_nucleosomes.py
+++ neptuner/thresh_nucleosomes.py
@@ -1,44 +1,62 @@
 import sys, os
 import pandas as pd
 import numpy as np
-from peakdetect import peakdetect
 
-input = '/home/pejo/Scrivania/nept_pattern/profiles/profile1.csv'
-output = "/home/pejo/Scrivania/prova.csv"
+###-MAIN FUNCTION-###
+def thresh_nucleosomes(ipt, opt, thr):
+    ## Identify nucleosome coordinates from profile, exploting a threshold value
+
+    # Assertions on parameters
+    # Input is a string and an existing file
+    assert isinstance(ipt, str)
+    assert os.path.isfile(ipt)
+    # Output is a string
+    assert isinstance(opt, str)
+    # thresh is a float in the range [0, 1]
+    assert isinstance(thr, float)
+    assert 0 < thr < 1
+
+    # reads profile file
+    prof = pd.read_csv(ipt, header=None)
+    assert prof.shape[1] == 4
+    assert prof[0].unique().size == 1
+    assert isinstance(prof[0][0], str)
+    # Second column (start) is integer
+    occ = np.array(prof[1])
+    assert all([isinstance(occ[i], np.int64) for i in range(prof.shape[0])])
+    # Third column (end) is integer
+    occ = np.array(prof[2])
+    assert all([isinstance(occ[i], np.int64) for i in range(prof.shape[0])])
+    # Third column (end) is integer
+    occ = np.array(prof[3])
+    assert all([isinstance(occ[i], np.float64) for i in range(prof.shape[0])])
+
+    # Thresholding
+    occ[occ < thr] = 0
+    # Retrieve coordinates
+    nucleosomes = coordinates(occ)
+    # Write output file
+    pd.DataFrame({'start': nucleosomes[0], 'end': nucleosomes[1], "length": nucleosomes[2]}).to_csv(opt)
+    return
+
+###-INTERNAL FUNCTIONS-###
+
+def coordinates(pr):
+    # Retrieve the signal above the threshold coordinates
+    pos = np.where(pr > 0)[0]
+    idx = np.append(True, np.diff(pr) > 1)
+    st = pos[idx] + 1
+    en = pos[idx]
+    ln = en - st
+    return np.array([st, en, ln])
+
+###-END FUNCTIONS-###
+
+#input = '/home/pejo/Scrivania/chr1.bedgraph'
+#output = "/home/pejo/Scrivania/prova.csv"
+# User parameters
+input = sys.argv[1]
+output = sys.argv[2]
 thresh = sys.argv[3]
-
-assert isinstance(input, str)
-assert os.path.isfile(input)
-assert isinstance(output, str)
-assert isinstance(thresh, float)
-assert 0 < thresh < 1
-
-# reads profile file
-prof = pd.read_csv(input, header=None)
-assert len(prof.columns) == 1
-assert prof[0].dtype == "float"
-
-
-occ = np.asarray(prof[0])
-occ[occ < thresh] = 0
-
-
-# Retrieve the nucleosome coordinates
-pos = np.where(occ > 0)[0]
-idx = np.append(True, np.diff(pos) > 1)
-st = pos[idx] + 1
-
-pos = np.where(occ > 0)[0]
-idx = np.append(np.diff(pos) > 1, True)
-en = pos[idx] + 139
-ln = en - st
-
-peaks = peakdetect(occ, lookahead=50)
-indexesMax = np.array([peaks[0][el][0] for el in range(len(peaks[0]))])
-indexesMax = indexesMax[np.array([np.where(indexesMax > st[i])[0][0] for i in range(st.size-1)])]
-peak = np.zeros(st.size-1)
-peak[np.where( (indexesMax > st[:-1]) & (indexesMax < en[:-1]) & (ln[:-1] < 150))] = \
-    indexesMax[(indexesMax > st[:-1]) & (indexesMax < en[:-1]) & (ln[:-1] < 150)]
-
-
-pd.DataFrame({'start': st[:-1], 'end': en[:-1], "length": ln[:-1], "peak": peak}).to_csv(output)
+# Call main function
+thresh_nucleosomes(input, output, thresh)
